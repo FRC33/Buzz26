a70d65ae8ba877e6e3c8dff796a85833cf6447a6
Nolan Kuza <nkuza8@gmail.com>
2021-03-08 11:28:45 -0500

    Finish SwerveModule skeleton

--- src/main/java/frc2020/subsystems/SwerveModule.java
+++ src/main/java/frc2020/subsystems/SwerveModule.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 
 import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.Timer;
+import frc2020.subsystems.Hood.CommandMode;
 import lib.drivers.BuzzTalonFX;
 import lib.drivers.TalonFXFactory;
+import lib.geometry.Rotation2d;
 import lib.subsystems.Subsystem;
 
 /**
@@ -28,7 +30,7 @@ public class SwerveModule extends Subsystem {
     public enum SteerMode {
         DISABLED,
         VOLTAGE,
-        ANGLE
+        ANGLE /** Motion magic */
     }
 
     public static class SwerveModuleConstants {
@@ -37,10 +39,24 @@ public class SwerveModule extends Subsystem {
         public int kSteerEncoderId = 0; /** DIO channel */
 
         public double kDriveMotorGearReduction;
+        public double kDriveWheelDiameter = 6; /** in */
+        //TODO add current limiting
+        public double kDriveKp;
+        public double kDriveKi;
+        public double kDriveKd;
+        public double kDriveKf;
+        public double kDriveKiZone = 0;
 
         public double kSteerMotorGearReduction;
-        
         public double kSteerEncoderGearReduction;
+        //TODO add current limiting
+        public double kSteerKp;
+        public double kSteerKi;
+        public double kSteerKd;
+        public double kSteerKf;
+        public double kSteerKiZone = 0;
+        public double kSteerKv; /** Feedforward velocity */
+        public double kSteerKa; /** Feedforward acceleration */
     }
 
     private SwerveModule(SwerveModuleConstants constants) {
@@ -63,15 +79,16 @@ public class SwerveModule extends Subsystem {
         public double timestamp;
         // Drive
         public double driveSupplyVoltage;
-        public double driveVelocity;
+        public double driveVelocity; /** in/s */
         // Steer
-        public double absoluteAngle;
-        public double relativeAngle;
-        public double trackedAngle;
+        public double steerSupplyVoltage;
+        public double absoluteAngle; /** Degrees from magnetic encoder */
+        public double relativeAngle; /** Degrees from steer Falcon integrated sensor. Not wrapped */
+        public double trackedAngle; /** Not wrapped */
 
         // OUTPUTS
         public double driveCommand;
-        public double steerCommand;
+        public double steerCommand; /** Wrapped -180 to 180 */
         public DriveMode driveMode;
         public SteerMode steerMode;
     }
@@ -90,6 +107,35 @@ public class SwerveModule extends Subsystem {
         
     }
 
+    public double getVelocity() {
+        return mPeriodicIO.driveVelocity;
+    }
+
+    public Rotation2d getAngle() {
+        return Rotation2d.fromDegrees(mPeriodicIO.trackedAngle);
+    }
+
+    public void disable() {
+        mPeriodicIO.driveCommand = 0;
+        mPeriodicIO.driveMode = DriveMode.DISABLED;
+        mPeriodicIO.steerCommand = 0;
+        mPeriodicIO.steerMode = SteerMode.DISABLED;
+    }
+
+    public void setVoltage(double voltage) {
+        mPeriodicIO.driveCommand = voltage;
+        mPeriodicIO.driveMode = DriveMode.VOLTAGE;
+    }
+
+    public void setAngle(double angle) {
+        mPeriodicIO.steerCommand = angle;
+        mPeriodicIO.steerMode = SteerMode.ANGLE;
+    }
+
+    public void setAngle(Rotation2d rotation2d) {
+        setAngle(rotation2d.getDegrees());
+    }
+
     @Override
     public void stop() {
     }
@@ -101,7 +147,7 @@ public class SwerveModule extends Subsystem {
 
     @Override
     public void outputTelemetry() {
-
+        
     }
     
 }
