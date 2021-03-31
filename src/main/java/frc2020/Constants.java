793a903cc3ef6ad98e8c14d18c549788bc5b9bec
Nolan Kuza <nkuza8@gmail.com>
2021-03-30 21:45:54 -0400

    Adjust teleop drive constants

--- src/main/java/frc2020/Constants.java
+++ src/main/java/frc2020/Constants.java
@@ -64,8 +64,13 @@ public final class Constants {
     public static final double kPathThetaMaxVelocity = 100 / 16;
     public static final double kPathThetaMaxAcceleration = 400 / 16;
 
-    public static final double kDriveMaxLinearVelocity = Units.inchesToMeters(110);
-    public static final double kDriveMaxAngularVelocity = 75 / 16; // Units do not matter
+    /*
+    145 - bounce
+    145 - hyperdrive
+    200 - barrel
+    */
+    public static final double kDriveMaxLinearVelocity = Units.inchesToMeters(150);
+    public static final double kDriveMaxAngularVelocity = 45 / 16; // Units do not matter
     
     public static final double kDriveJoystickDeadbandCutoff = 0.08;
     public static final double kDriveJoystickWeight = 0.5;
